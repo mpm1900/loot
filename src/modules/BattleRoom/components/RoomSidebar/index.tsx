27619df672fe4c8f87ddee3afcd01023e80a9391
snax <almostmacmiller@gmail.com>
2019-02-12 23:11:56 -0600

    component refactors

---
+++ src/modules/BattleRoom/components/RoomSidebar/index.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import { List } from 'immutable';
+import { TopBar } from '../../../Core/TopBar'
+import { Icon } from '../../../Core/Icon'
+import RoomChat from '../RoomChat'
+import './index.scss'
+
+enum SidebarKey {
+    Users = 'Users',
+    Chat = 'Chat',
+    Battlelog = 'Battlelog',
+    Settings = 'Settings',
+}
+
+const keyIcons = {
+    [SidebarKey.Users]: 'two-shadows',
+    [SidebarKey.Chat]: 'talk',
+    [SidebarKey.Battlelog]: 'audio-cassette',
+    [SidebarKey.Settings]: 'cog',
+}
+
+export const RoomSidebar = (props: any) => {
+    const [ activeKey, setActiveKey ] = useState(SidebarKey.Chat)
+    const keys = List.of(SidebarKey.Chat, SidebarKey.Battlelog, SidebarKey.Users, SidebarKey.Settings)
+    const isActive = (key: SidebarKey) => key === activeKey
+    const fill = key => isActive(key) ? 'rgba(255,255,255,0.84)' : 'rgba(255,255,255,0.54)'
+    const selectStyle = { width: '25%', display: 'flex', justifyContent: 'center' }
+
+    const sidebar = (key: string, props: any) => {
+        switch (key) {
+            case SidebarKey.Users: return <div>USERS</div>
+            case SidebarKey.Chat: return <RoomChat />
+            case SidebarKey.Battlelog: return <div>BATTLELOG</div>
+            default: return <div></div>
+        }
+    }
+
+    return (
+        <div className='RoomSidebar'>
+            <TopBar style={{ borderLeft: 'none', borderRight: 'none' }}>
+                {keys.map(key => (
+                    <div style={selectStyle} onClick={() => setActiveKey(key)}>
+                        <Icon size={32} icon={keyIcons[key]} fill={fill(key)} />
+                    </div>
+                ))}
+            </TopBar>
+            <div className='RoomSidebar__body'>
+                {sidebar(activeKey, props)}
+            </div>
+        </div>
+)
+}
--- src/modules/BattleRoom/index.tsx
+++ src/modules/BattleRoom/index.tsx
@@ -1,34 +1,18 @@
-import React, { useEffect, useRef, useState, CSSProperties } from 'react'
+import React, { useEffect, useState, CSSProperties } from 'react'
 import './index.scss'
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { BattleCharacter } from './components/BattleCharacter';
-import { requestCreateRoom, requestLeaveRoom, requestJoinRoom, requestFindRoom, readyUser, cancelReady } from '../../state/actions/room.actions';
-import { TopBar } from '../Core/TopBar';
-import { Icon } from '../Core/Icon';
-import RoomChat  from './components/RoomChat';
-import '../../components/Icon/icons.svg';
-import { Button } from '../Core/Button';
-import Modal, { Styles } from 'react-modal'
-import Session from '../Session';
-import { Input } from '../Core/Input';
-
-enum SidebarKey {
-    Users = 'Users',
-    Chat = 'Chat',
-    Battlelog = 'Battlelog',
-    Settings = 'Settings',
-}
-
-const keyIcons = {
-    [SidebarKey.Users]: 'two-shadows',
-    [SidebarKey.Chat]: 'talk',
-    [SidebarKey.Battlelog]: 'audio-cassette',
-    [SidebarKey.Settings]: 'cog',
-}
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { BattleCharacter } from './components/BattleCharacter'
+import { requestCreateRoom, requestLeaveRoom, requestJoinRoom, requestFindRoom, readyUser, cancelReady } from '../../state/actions/room.actions'
+import { TopBar } from '../Core/TopBar'
+import '../../components/Icon/icons.svg'
+import { Button } from '../Core/Button'
+import Modal from 'react-modal'
+import Session from '../Session'
+import { Input } from '../Core/Input'
+import { RoomSidebar } from './components/RoomSidebar'
 
 export const Room = (props: any) => {
-    const [ sidebarKey, setSidebarKey ] = useState(SidebarKey.Chat)
     const [ roomId, setRoomId ] = useState(null)
     const [ sessionModalOpen, setSessionModalOpen ] = useState(false)
     const { auth, session, history, requestCreateRoom, requestJoinRoom, requestLeaveRoom, requestFindRoom, room, users, location, readyUser, cancelReady } = props
@@ -43,21 +27,12 @@ export const Room = (props: any) => {
         if (location.pathname === '/battle/find')
             requestFindRoom()
         return () => { requestLeaveRoom() }
-    }, [])    
-
-    const sidebar = (key: string, props: any) => {
-        switch (key) {
-            case SidebarKey.Users: return <div>USERS</div>
-            case SidebarKey.Chat: return <RoomChat />
-            case SidebarKey.Battlelog: return <div>BATTLELOG</div>
-            default: return <div></div>
-        }
-    }
+    }, [])
 
     const joinRoom = (roomId) => roomId ? requestJoinRoom(roomId) : null
-    const isActive = (key) => key === sidebarKey
     const isUser = (userId) => room.users.map(u => u.id).contains(userId)
     const isReady = (userId) => room.readyUserIds.contains(userId)
+    const isSecret = (userId) => userId !== session.userId
     const getUserClass = (user) => {
         if (user) {
             if (user.id === room.creatorId) return 'creator'
@@ -81,88 +56,127 @@ export const Room = (props: any) => {
         borderBottom: '1px solid black', 
         justifyContent: 'flex-start' 
     }
+    const characterListStyle = {
+        display: 'flex', 
+        flex: 1, 
+        border: '1px solid rgba(255,255,255,0.12)', 
+        justifyContent: room.playerSessions.size > 1 ? 'space-between' : 'flex-start', 
+        boxSizing: 'border-box', 
+        overflowY: 'auto', 
+        background: 'linear-gradient(175deg, hsl(0,0%,27%) 0%,hsl(0,0%,22%) 100%)'
+    }
+    const activeCharacterStyle = (index) => ({
+        padding: 16,
+        boxShadow: index === 0 ? '1px 0px 5px black inset' : '-1px 0px 5px black inset',
+        overflow: 'hidden',
+        border: '1px solid rgba(255,255,255,0.24)',
+        boxSizing: 'border-box',
+        width: 'calc(100% - 1px)',
+        background: index === 0 ?
+            'linear-gradient(135deg, hsl(0,0%,20%) 0%,hsl(0,0%,10%) 100%)':
+            'linear-gradient(-45deg, hsl(0,0%,20%) 0%,hsl(0,0%,10%) 100%)'
+    })
+
+    const RoomTopbar = () => (
+        <TopBar>
+            <div className='roomid'>
+                <strong>{room.id}</strong>
+            </div>
+            <div style={{display: 'flex', alignItems: 'flex-end'}}>
+                <Input className='Room__search' placeholder={'enter room id'} value={roomId} onChange={event => setRoomId(event.target.value)} />
+                <Button type='secondary' onClick={() => joinRoom(roomId)}>Join Room</Button>
+                <Button type='secondary' onClick={() => history.push('/')}>Leave Room</Button>
+                <Button type='secondary' onClick={() => setSessionModalOpen(true)}>Edit Party</Button>
+                { isUser(session.userId) && !isReady(session.userId) ? <Button onClick={() => readyUser()}>Ready Up</Button> : null }
+                { isUser(session.userId) && isReady(session.userId) ? <Button onClick={() => cancelReady()} type='warning'>Cancel</Button> : null }
+            </div>
+        </TopBar>
+    )
+
+    const RoomUserbar = () => (
+        <TopBar condensed={true} style={{ borderTop: 'none', borderLeft: 'none', borderRight: 'none' }}>
+            {room.playerSessions.map((pSession, index) => {
+                const user = room.users.find(u => u.id === pSession.userId)
+                return (
+                    <span key={pSession.sessionId} className={'Battle__user--name' + ' ' + getUserClass(user)}>
+                        {isReady(user.id) ? 'READY' : null} {user.username}
+                    </span>
+                )
+                })}
+        </TopBar>
+    )
+
+    const RoomActionbar = () => (
+        <TopBar style={actionBarStyle}>
+            <div style={{ display: 'flex' }}>
+                <Button type='primary'>Weapon Attack</Button>
+                <Button type='important'>Skill 1</Button>
+                <Button type='important'>Skill 2</Button>
+                <Button type='important'>Skill 3</Button>
+            </div>
+            <div style={{ display: 'flex' }}>
+                <Button type='info'>Switch Heros</Button>
+            </div>
+        </TopBar>
+    )
+
+    const SessionModal = () => {
+        const style = {
+            content: {
+                background: 'white',
+                zIndex: 9999,
+                padding: 0,
+                display: 'flex',
+                flexDirection: 'column',
+            },
+            overlay: {
+                backgroundColor: 'rgba(0,0,0,0.52)'
+            }
+        }
+        
+        return (
+            <Modal isOpen={sessionModalOpen} onRequestClose={() => setSessionModalOpen(false)} style={style} >
+                <Session isModal={true} onClose={() => setSessionModalOpen(false)} />
+            </Modal>
+        )
+    }
 
     return (
         room && room.playerSessions ? <div className='Battle'>
-            <TopBar>
-                <div className='roomid'>
-                    <strong>{room.id}</strong>
-                </div>
-                <div style={{display: 'flex', alignItems: 'flex-end'}}>
-                    <Input className='Room__search' placeholder={'enter room id'} value={roomId} onChange={event => setRoomId(event.target.value)} />
-                    <Button type='secondary' onClick={() => joinRoom(roomId)}>Join Room</Button>
-                    <Button type='secondary' onClick={() => history.push('/')}>Leave Room</Button>
-                    <Button type='secondary' onClick={() => setSessionModalOpen(true)}>Edit Party</Button>
-                    { isUser(session.userId) && !isReady(session.userId) ? <Button onClick={() => readyUser()}>Ready Up</Button> : null }
-                    { isUser(session.userId) && isReady(session.userId) ? <Button onClick={() => cancelReady()} type='warning'>Cancel</Button> : null }
-                </div>
-            </TopBar>
+            <RoomTopbar />
             <div className='Battle__body'>
                 <div style={battleBodyBorderStyle as CSSProperties}>
-                    <TopBar condensed={true} style={{ borderTop: 'none', borderLeft: 'none', borderRight: 'none' }}>
-                        {room.playerSessions.map((pSession, index) => {
-                            const user = room.users.find(u => u.id === pSession.userId)
-                            return (
-                                <span key={pSession.sessionId} className={'Battle__user--name' + ' ' + getUserClass(user)}>
-                                    {isReady(user.id) ? 'READY' : null} {user.username}
-                                </span>
-                            )
-                         })}
-                    </TopBar>
-                    <TopBar style={actionBarStyle}>
-                        <div style={{ display: 'flex' }}>
-                            <Button type='important'>Weapon Attack</Button>
-                            <Button type='important'>Skill 1</Button>
-                            <Button type='important'>Skill 2</Button>
-                            <Button type='important'>Skill 3</Button>
-                            <Button type='important'>Switch</Button>
-                        </div>
-                    </TopBar>
-                    <div style={{ display: 'flex', flex: 1, border: '1px solid rgba(255,255,255,0.12)', justifyContent: room.playerSessions.size > 1 ? 'space-between' : 'flex-start', boxSizing: 'border-box', overflowY: 'auto', background: 'linear-gradient(175deg, hsl(0,0%,27%) 0%,hsl(0,0%,22%) 100%)' }}>
-                        {room.playerSessions.map((pSession, index) => <div style={{width: '50%'}}>
-                            <div className='Battle__user'>
-                                {pSession.party.characters.get(0) ? 
-                                <div style={{
-                                    padding: 16,
-                                    boxShadow: index === 0 ? '1px 0px 5px black inset' : '-1px 0px 5px black inset',
-                                    overflow: 'hidden',
-                                    border: '1px solid rgba(255,255,255,0.24)',
-                                    boxSizing: 'border-box',
-                                    width: 'calc(100% - 1px)',
-                                    background: index === 0 ?
-                                        'linear-gradient(135deg, hsl(0,0%,20%) 0%,hsl(0,0%,10%) 100%)':
-                                        'linear-gradient(-45deg, hsl(0,0%,20%) 0%,hsl(0,0%,10%) 100%)'
-                                }}><BattleCharacter reverse={index === 1} 
-                                    active={true} 
-                                    character={pSession.party.characters.get(0)} secret={pSession.userId !== session.userId} 
-                                /></div>: null }
-                                {pSession.party.characters.shift().map(character => 
-                                    <div style={{padding: 8}}><BattleCharacter reverse={index === 1} 
-                                        active={false} 
-                                        character={character} secret={pSession.userId !== session.userId} 
-                                    /></div>
-                                )}
-                            </div>
-                        </div>)}
-                    </div>
-                </div>
-                <div className='Battle__sidebar'>
-                    <TopBar style={{ borderLeft: 'none', borderRight: 'none' }}>
-                        {[SidebarKey.Chat, SidebarKey.Battlelog, SidebarKey.Users, SidebarKey.Settings].map(key => (
-                            <div style={{
-                                    width: '25%', 
-                                    display: 'flex', 
-                                    justifyContent: 'center'
-                                }} 
-                                onClick={() => setSidebarKey(key)}>
-                                    <Icon size={32} icon={keyIcons[key]} fill={isActive(key) ? 'rgba(255,255,255,0.84)' : 'rgba(255,255,255,0.54)'} />
+                    <RoomUserbar />
+                    <RoomActionbar /> 
+                    <div style={characterListStyle as CSSProperties}>
+                        {room.playerSessions.map((pSession, index) => (
+                            <div style={{ width: '50%' }}>
+                                <div className='Battle__user'>
+                                    {pSession.party.characters.get(0) ? 
+                                        <div style={activeCharacterStyle(index) as CSSProperties}>
+                                            <BattleCharacter 
+                                                reverse={index === 1} 
+                                                active={true} 
+                                                character={pSession.party.characters.get(0)} secret={isSecret(pSession.userId)} 
+                                            />
+                                        </div>: 
+                                        null 
+                                    }
+                                    {pSession.party.characters.shift().map(character => 
+                                        <div style={{padding: 8}}>
+                                            <BattleCharacter 
+                                                reverse={index === 1} 
+                                                active={false} 
+                                                character={character} secret={isSecret(pSession.userId)} 
+                                            />
+                                        </div>
+                                    )}
+                                </div>
                             </div>
                         ))}
-                    </TopBar>
-                    <div className='Battle__sidebar__body'>
-                        {sidebar(sidebarKey, props)}
                     </div>
                 </div>
+                <RoomSidebar />
             </div>
             <Modal
                 isOpen={sessionModalOpen}
@@ -179,9 +193,9 @@ export const Room = (props: any) => {
                         backgroundColor: 'rgba(0,0,0,0.52)'
                     }
                 }}  
-                >
-                    <Session isModal={true} onClose={() => setSessionModalOpen(false)} />
-                </Modal>
+            >
+                <Session isModal={true} onClose={() => setSessionModalOpen(false)} />
+            </Modal>
         </div> : null
     )
 }
